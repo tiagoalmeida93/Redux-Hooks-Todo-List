43a5050d53d24b3a1312611b80720f62987ca8f4
Tiago Almeida <tiago.almeida@ies2.com.br>
2020-03-26 16:09:09 -0300

    Actions creators e connect no component todo

--- src/components/TodoList.jsx
+++ src/components/TodoList.jsx
@@ -1,16 +1,39 @@
-import React from 'react';
+import React, { useState } from 'react';
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
 
-// import { Container } from './styles';
+import * as todosAction from '../actions/todos';
 
-export default function TodoList() {
+function TodoList(props) {
+  const [newTodoText, setNewTodoText] = useState('');
+  console.log(props)
+
+  function addNewTodo() {
+    props.addTodo(newTodoText)
+    setNewTodoText('')
+  }
+
   return (
     <>
       <ul>
         <li>Item do todo</li>
       </ul>
 
-      <input type="text"/>
-      <button>Novo Todo</button>
+      <input 
+        type="text"
+        value={newTodoText}
+        onChange={e => setNewTodoText(e.target.value)}
+      />
+      <button onClick={() => addNewTodo()}>Novo Todo</button>
     </>
   );
 }
+
+const mapStateToProps = state => ({
+  todos: state.todos,
+});
+
+const mapDispatchToProps = dispatch =>
+  bindActionCreators(todosAction, dispatch);
+
+export default connect(mapStateToProps, mapDispatchToProps)(TodoList)
